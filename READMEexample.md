1ff6edaf562ce53c3042554f9f739e6a8307fd99
Michael Medrano <mgmedrano@outlook.com>
2021-08-10 23:50:35 -0700

    updated index.js, deleted README and gerateMarkdown.js, READMEexample.md created after node with video

---
+++ READMEexample.md
@@ -0,0 +1,34 @@
+# Mike's New Project
+![](https://img.shields.io/badge/license-MIT%20License-blue?style=flat-square)
+## Description
+A New Project for Mike
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contribution](#contribution)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+## Installation
+1. install npm
+2. npm package inquirer
+3. standard package fs
+
+## Usage
+Used for Mike's New Project
+
+## Contribution
+Reach out to me via email to contribute to this project
+
+## Tests
+This is where you would test, if applicable
+
+## Questions
+For questions about this project, please see my GitHub at [mgmedrano](https://github.com/mgmedrano), or reach out by email at mgmedrano0203@gmail.com.
+
+## License
+Licensed under MIT License.
+
+## Video Preview
+<iframe src="https://drive.google.com/file/d/1tzkwNHfLr_3SMq4HMZcdA_HxHj0ihpgQ/preview" width="500" height="550"></iframe>
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// TODO: Include packages needed for this application
+//requiring specific npm package inquirer 
 const inquirer = require('inquirer');
+//requiring node standard library for writing file
 const fs = require('fs');
 
 inquirer
@@ -11,32 +12,32 @@ inquirer
         {
             type: 'input',
             name: 'description',
-            message: 'Enter the description of the project:',
+            message: 'Enter Project description:',
         },
         {
             type: 'input',
             name: 'install',
-            message: 'Enter installation instructions (separate using commas):',
+            message: 'Enter installation instructions (use commas for numbered list):',
         },
         {
             type: 'input',
             name: 'usage',
-            message: 'Enter usage information for the project:',
+            message: 'Enter Project usage information:',
         },
         {
             type: 'input',
             name: 'contribution',
-            message: 'Enter contribution guidelines for the project',
+            message: 'Enter Project contribution guidelines:',
         },
         {
             type: 'input',
             name: 'test',
-            message: 'Enter test information for the project',
+            message: 'Enter Project test information:',
         },
         {
             type: 'input',
             name: 'gitHub',
-            message: 'Enter your GitHub Username:',
+            message: 'What is your GitHub Username?',
         },
         {
             type: 'input',
@@ -44,23 +45,122 @@ inquirer
             message: 'What is your email address?',
         },
         {
-            type: 'input',
-            name: 'gitHub',
-            message: 'Enter your GitHub Username:',
-        },
+          type: "list",
+          name: "license",
+          choices: [
+              "MIT License",
+              "Attribution-NonCommercial 4.0 International",
+              "Eclipse Public License 1.0",
+              "IBM Public License Version 1.0",
+              "Mozilla Public License 2.0"
+          ]
+      }
     ])
-.then((data) => {
+    .then((res) => {
+        console.log("Generating README.md...");
+        createREADMEFile(res);
+        
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
-  fs.writeFile('README.md' , generateMarkdown(JSON.stringify(data)), (err) =>
-      err ? console.log(err) : console.log('Success!'))
-// TODO: Create an array of questions for user input
-const questions = [];
+    function createREADMEFile(input) {
+        let readmeTitle;
+        let readmeDescription;
+        const descriptionHeader = "## Description";
+        let tableOfContents;
+        const TOCHeader = "## Table of Contents";
+        let installArr;
+        const installHeader = "## Installation";
+        let readmeUsage;
+        const usageHeader = "## Usage";
+        let readmeContribution;
+        const contributionHeader = "## Contribution";
+        let readmeTest;
+        const testingHeader = "## Tests";
+        let readmeLicence = input.license;
+        const licenseHeader = "## License";
+        let readmeQuestions;
+        const questionsHeader = "## Questions";
+        let completeREADME = [];
+        
+        //writing title section from user input
+        if (input.title == '') {
+            readmeTitle = '# TITLE';
+        } else {
+            readmeTitle = `# ${input.title}`;
+        }
+        completeREADME.push(readmeTitle);
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+        //placing badge license
+        let badge = `![](https://img.shields.io/badge/license-${readmeLicence.replace(/ /g, "%20")}-blue?style=flat-square)`;
+        completeREADME.push(badge);
 
-// TODO: Create a function to initialize app
-function init() {}
+        // writing description section from user input
+        if (input.description == '') {
+            readmeDescription = `${descriptionHeader}\n Enter project description here.`;
+        } else {
+            readmeDescription = `${descriptionHeader}\n${input.description}`;
+        }
+        completeREADME.push(readmeDescription);
 
-// Function call to initialize app
-init();
+        //writing the Table of Contents (TOC)
+        tableOfContents = `${TOCHeader}\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contribution](#contribution)\n* [Tests](#tests)\n* [License](#license)\n* [Questions](#questions)\n`;
+        completeREADME.push(tableOfContents);
+
+        //writing the install from user input
+        completeREADME.push(`${installHeader}`);
+        
+        installArr = input.install.split(',').map(item => {
+            return `${item.trim()}`;
+        });
+        
+        for (var i = 0; i < installArr.length; i++) {
+            completeREADME.push(`${i + 1}. ${installArr[i]}`);
+        }
+        
+        //writing usage from user input
+        if (input.usage == '') {
+            readmeUsage = `\n${usageHeader}\n Enter project usage here.`;
+        } else {
+            readmeUsage = `\n${usageHeader}\n${input.usage}`;
+        }
+        completeREADME.push(readmeUsage);
+        
+        //writing the Contributing from user input
+        if (input.contribution == '') {
+            readmeContribution = `\n${contributionHeader}\n Enter project contribution.`;
+        } else {
+            readmeContribution = `\n${contributionHeader}\n${input.contribution}`;
+        }
+        completeREADME.push(readmeContribution);
+
+        //writing test from user input
+            if (input.testing == '') {
+                readmeTest = `\n${testingHeader}\n Enter project testing information here.`;
+            } else {
+                readmeTest = `\n${testingHeader}\n${input.test}`;
+            }
+            completeREADME.push(readmeTest);
+        
+        //writing questions section from user input of GitHub and email
+        readmeQuestions = `\n${questionsHeader}\nFor questions about this project, please see my GitHub at [${input.gitHub}](https://github.com/${input.gitHub}), or reach out by email at ${input.email}.`;
+        completeREADME.push(readmeQuestions);
+               
+        //License info
+        readmeLicence = `\n${licenseHeader}\nLicensed under ${input.license}.`;
+        completeREADME.push(readmeLicence);
+            
+        //putting together README from user inputs
+        const README = completeREADME.join('\n');
+         
+        //node standard library File System to created file READMEexample.md
+        fs.writeFile("READMEexample.md", README, (err) => {
+            if (err) {
+                throw err;
+            } else {
+                console.log("README file successfully created!");
+            }
+        });
+    }
